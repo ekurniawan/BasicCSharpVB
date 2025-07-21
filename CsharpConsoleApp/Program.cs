3efb51a5c71c158b4dc0b3dba8bb7547ac8a8d5f
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-07-21 11:06:10 +0700

    Add project files.

---
+++ CsharpConsoleApp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nama = "John Doe";
+
+            int a = 12;
+            int b = 8;
+
+            Console.WriteLine("b: " + b);
+            int c = b;
+            Console.WriteLine("c: " + c);
+            b = 10;
+            Console.WriteLine("c: " + c);
+
+
+            Person person1 = new Person("Alice", 30);
+            Person person2 = new Person();
+            person2.Name = "Bob";
+            person2.Age = 25;
+
+        }
+    }
+
+    public class Person
+    {
+        //public string Name { get; set; }
+
+        /*private string name;
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string value)
+        {
+            name = value;
+        }*/
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                name = value;
+            }
+        }
+
+
+
+        public int Age { get; set; }
+
+        public Person()
+        { }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+}
---
+++ VBConsoleApp/Module1.vb
@@ -0,0 +1,22 @@
+﻿
+
+Module Module1
+
+    Sub Main()
+        Dim nama As String = "John Doe"
+        ' Display a greeting message to the user
+
+        Console.WriteLine("Hello, " & nama & "! Welcome to the VB Console Application.")
+
+        Dim a As Integer = 5
+        Dim b As Integer = 10
+
+        Dim c = b
+        Console.WriteLine("The value of c is: " & c)
+        b = 15
+        Console.WriteLine("The value of b is now: " & c)
+
+
+    End Sub
+
+End Module
